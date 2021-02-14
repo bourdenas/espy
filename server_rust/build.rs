7a441708d7707e3b2feb8fe7265fd04b62116861
Themis <bourdenas@gmail.com>
2021-02-14 23:14:11 +0100

    Remove unnecessary reconciliation_task.proto from codegen.

--- server_rust/build.rs
+++ server_rust/build.rs
@@ -3,7 +3,6 @@ fn main() {
         &[
             "../server/proto/igdbapi.proto",
             "../server/proto/library.proto",
-            "../server/proto/reconciliation_task.proto",
             "../server/proto/steam_entry.proto",
         ],
         &["../server/proto/"],
