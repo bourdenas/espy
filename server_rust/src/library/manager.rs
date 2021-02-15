ee3ea063103fbfc611c4ca826dbefbe11a150574
Themis <bourdenas@gmail.com>
2021-02-15 21:58:56 +0100

    Retrieve Steam game in Library manager.
    Separates game entries that have not been recon'd in library yet.

--- server_rust/src/library/manager.rs
+++ server_rust/src/library/manager.rs
@@ -2,6 +2,7 @@ use crate::espy;
 use crate::recon;
 use crate::steam;
 use crate::util;
+use std::collections::HashSet;
 
 pub struct LibraryManager {
     pub library: espy::Library,
@@ -46,7 +47,42 @@ impl LibraryManager {
             }
         });
 
+        let steam_entries = match &self.steam_api {
+            Some(api) => api.get_owned_games().await?,
+            None => espy::SteamList {
+                ..Default::default()
+            },
+        };
+
         self.library = lib_future.await.unwrap();
+
+        let non_lib_entries = self.get_non_library_entries(steam_entries);
+        println!("non_lib_entries: {:?}", non_lib_entries);
         Ok(())
+    }
+
+    // Filters Steam game entries that are not included in the manager's library.
+    fn get_non_library_entries(&self, entries: espy::SteamList) -> Vec<espy::SteamEntry> {
+        // Collect steam ids of reconciled entries.
+        let lib_ids =
+            self.library
+                .entry
+                .iter()
+                .filter_map(|e| {
+                    // Find Steam entry in stores owned and returns the game's steam id.
+                    match e.store_owned.iter().find(|store| {
+                        store.store_id == espy::game_entry::store::StoreId::Steam as i32
+                    }) {
+                        Some(store) => Some(store.game_id),
+                        None => None,
+                    }
+                })
+                .collect::<HashSet<i64>>();
+
+        entries
+            .game
+            .into_iter()
+            .filter(|e| !lib_ids.contains(&e.id))
+            .collect()
     }
 }
