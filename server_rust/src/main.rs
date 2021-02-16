280c892368f31f91ded6033c93920c9ecf365b98
Themis <bourdenas@gmail.com>
2021-02-16 22:49:38 +0100

    Add some basic HTML rendering for the library.

--- server_rust/src/main.rs
+++ server_rust/src/main.rs
@@ -28,11 +28,57 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     )
     .await?;
 
-    println!(
-        "entries: {}\nunreconciled entries: {}",
-        mgr.library.entry.len(),
-        mgr.library.unreconciled_steam_game.len()
-    );
+    render_html(&mgr.library)?;
+
+    Ok(())
+}
+
+use std::fs::File;
+use std::io::Write;
+
+// Basic rendering of library in HTML.
+fn render_html(library: &espy::Library) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    const HTML_CODE: &str = r#"
+    <html>
+    <head>
+      <style>
+        .app {
+          display: grid;
+          grid-gap: 15px;
+          overflow: hidden;
+          grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+          grid-auto-flow: dense;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="app">"#;
+    const HTML_TAIL: &str = r#"
+      </div>
+    </body>
+    </html>"#;
+
+    let lines = library.entry.iter().map(|e| match &e.game {
+        Some(game) => format!(
+            r#"<div class="item"><figure><p>
+                    <img src="https://images.igdb.com/igdb/image/upload/t_cover_big/{}.jpg">
+                    <figcaption><a href="{}">{}</a></figcaption></figure></div>"#,
+            match &game.cover {
+                Some(cover) => &cover.image_id,
+                None => "",
+            },
+            game.url,
+            game.name
+        ),
+        None => String::from(""),
+    });
+
+    let mut output = File::create(format!("target/{}_lib.html", TEST_USER))?;
+    writeln!(output, "{}", HTML_CODE)?;
+    for line in lines {
+        writeln!(output, "{}", line)?;
+    }
+    writeln!(output, "{}", HTML_TAIL)?;
 
     Ok(())
 }
