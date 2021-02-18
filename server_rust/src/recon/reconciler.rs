8ec61cb98cdf6a5628d2d552d53a61c76cb6d3f9
Themis <bourdenas@gmail.com>
2021-02-18 18:38:02 +0100

    Parallelise reconciliation of game entries with IGDB.
    Currently there is no rate limiting on requests to IGDB service and can easily exceed the 4 QPS limit of the free tier.

--- server_rust/src/recon/reconciler.rs
+++ server_rust/src/recon/reconciler.rs
@@ -1,14 +1,18 @@
 use crate::espy;
 use crate::igdb;
 use crate::igdb_service;
+use futures::future;
+use std::sync::Arc;
 
 pub struct Reconciler {
-    igdb: igdb_service::api::IgdbApi,
+    igdb: Arc<igdb_service::api::IgdbApi>,
 }
 
 impl Reconciler {
     pub fn new(igdb: igdb_service::api::IgdbApi) -> Reconciler {
-        Reconciler { igdb }
+        Reconciler {
+            igdb: Arc::new(igdb),
+        }
     }
 
     // Retrieve data from IGDB for Steam entries and create a Library based on
@@ -17,78 +21,94 @@ impl Reconciler {
         &self,
         steam_entries: &[espy::SteamEntry],
     ) -> Result<espy::Library, Box<dyn std::error::Error + Send + Sync>> {
+        let handles = steam_entries.iter().map(|entry| {
+            let igdb = Arc::clone(&self.igdb);
+            let entry = entry.clone();
+            tokio::spawn(async move {
+                match recon(&igdb, &entry).await {
+                    Ok(game) => match game {
+                        Some(game) => Ok(espy::GameEntry {
+                            game: Some(game),
+                            store_owned: vec![espy::game_entry::Store {
+                                game_id: entry.id,
+                                store_id: espy::game_entry::store::StoreId::Steam as i32,
+                            }],
+                        }),
+                        None => Err(entry),
+                    },
+                    Err(_) => {
+                        println!("failed recon request '{}'", entry.title);
+                        Err(entry)
+                    }
+                }
+            })
+        });
+        // TODO: This currently does not respect IGDB's free-tier 4 QPS and
+        // floods with errors for large slices.
+        let results = future::join_all(handles).await;
+
         let mut lib = espy::Library {
             ..Default::default()
         };
-
-        for entry in steam_entries {
-            match self.recon_entry(entry).await {
-                Ok(game) => match game {
-                    Some(game) => lib.entry.push(espy::GameEntry {
-                        game: Some(game),
-                        store_owned: vec![espy::game_entry::Store {
-                            game_id: entry.id,
-                            store_id: espy::game_entry::store::StoreId::Steam as i32,
-                        }],
-                    }),
-                    None => lib.unreconciled_steam_game.push(entry.clone()),
-                },
-                Err(_) => lib.unreconciled_steam_game.push(entry.clone()),
-            }
+        for result in results {
+            let result = result?;
+            match result {
+                Ok(game) => lib.entry.push(game),
+                Err(entry) => lib.unreconciled_steam_game.push(entry),
+            };
         }
 
         Ok(lib)
     }
+}
 
-    async fn recon_entry(
-        &self,
-        entry: &espy::SteamEntry,
-    ) -> Result<Option<igdb::Game>, Box<dyn std::error::Error + Send + Sync>> {
-        println!("recon '{}'", entry.title);
-        let result = match self.igdb.search_by_title(&entry.title).await {
-            Ok(r) => r,
-            Err(e) => {
-                println!("Failed to recon '{}': {}", &entry.title, e);
-                igdb::GameResult {
-                    ..Default::default()
-                }
+/// Returns a Game entry from IGDB that matches the input Steam entry.
+async fn recon(
+    igdb: &igdb_service::api::IgdbApi,
+    entry: &espy::SteamEntry,
+) -> Result<Option<igdb::Game>, Box<dyn std::error::Error + Send + Sync>> {
+    let result = match igdb.search_by_title(&entry.title).await {
+        Ok(r) => r,
+        Err(e) => {
+            println!("Failed to recon '{}': {}", &entry.title, e);
+            igdb::GameResult {
+                ..Default::default()
             }
-        };
-
-        let mut candidates = result
-            .games
-            .into_iter()
-            .map(|e| Candidate {
-                score: edit_distance(&entry.title, &e.name),
-                game: e,
-            })
-            .collect::<Vec<Candidate>>();
-        candidates.sort_by(|a, b| a.score.cmp(&b.score));
-
-        if candidates.len() == 0 {
-            return Ok(None);
         }
+    };
 
-        let mut game = candidates.pop().unwrap().game;
-        if let Some(cover) = game.cover {
-            game.cover = match self.igdb.get_cover(cover.id).await? {
-                Some(cover) => Some(Box::new(cover)),
-                None => None,
-            };
-        }
-        if let Some(collection) = game.collection {
-            game.collection = self.igdb.get_collection(collection.id).await?;
-        }
-        if game.franchises.len() > 0 {
-            game.franchises = self
-                .igdb
-                .get_franchises(&game.franchises.iter().map(|f| f.id).collect::<Vec<_>>())
-                .await?
-                .franchises;
-        }
+    let mut candidates = result
+        .games
+        .into_iter()
+        .map(|e| Candidate {
+            score: edit_distance(&entry.title, &e.name),
+            game: e,
+        })
+        .collect::<Vec<Candidate>>();
+    candidates.sort_by(|a, b| a.score.cmp(&b.score));
 
-        Ok(Some(game))
+    if candidates.len() == 0 {
+        return Ok(None);
     }
+
+    let mut game = candidates.pop().unwrap().game;
+    if let Some(cover) = game.cover {
+        game.cover = match igdb.get_cover(cover.id).await? {
+            Some(cover) => Some(Box::new(cover)),
+            None => None,
+        };
+    }
+    if let Some(collection) = game.collection {
+        game.collection = igdb.get_collection(collection.id).await?;
+    }
+    if game.franchises.len() > 0 {
+        game.franchises = igdb
+            .get_franchises(&game.franchises.iter().map(|f| f.id).collect::<Vec<_>>())
+            .await?
+            .franchises;
+    }
+
+    Ok(Some(game))
 }
 
 struct Candidate {
