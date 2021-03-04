dfdfa4dc9ca0c4fff2eb6e8e06375142bb03b65b
bourdenas <bourdenas@gmail.com>
2021-03-04 12:14:26 +0100

    Make navigation menu more responsive.
    Collapse NavigationRail when width becomes smaller and use a draw when viewport becomes very narrow.

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:espy/widgets/espy_drawer.dart' show EspyDrawer;
 import 'package:espy/widgets/espy_navigation_rail.dart' show EspyNavigationRail;
 import 'package:flutter/material.dart';
 
@@ -36,26 +37,31 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Row(children: [
-        EspyNavigationRail(true),
-        VerticalDivider(thickness: 1, width: 1),
-        Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('You have selected:'),
-              Text(
-                '$_selectedIndex',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ],
-          ),
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ]),
-    );
+        drawer: constraints.maxWidth <= 800 ? EspyDrawer() : null,
+        body: Row(children: [
+          if (constraints.maxWidth > 800) ...[
+            EspyNavigationRail(constraints.maxWidth > 1200),
+            VerticalDivider(thickness: 1, width: 1)
+          ],
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('You have selected:'),
+                Text(
+                  '$_selectedIndex',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ],
+            ),
+          ),
+        ]),
+      );
+    });
   }
 }
