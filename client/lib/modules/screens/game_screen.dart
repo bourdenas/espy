2e7ea4d9b34ad1dde6d21c590a761e3cad735e43
bourdenas <bourdenas@gmail.com>
2021-03-15 21:23:43 +0100

    Small fixes on game screen for larger displays.
    - Cover image does not expand.
    - Long game titles break lines.
    - Adjust screenshot aspect ration based on dimensions of first screenshot (shows artifacts if not all screenshots respect the same aspect ratio).

--- client/lib/modules/screens/game_screen.dart
+++ client/lib/modules/screens/game_screen.dart
@@ -47,35 +47,35 @@ class GameScreen extends StatelessWidget {
           padding: const EdgeInsets.all(16),
           child: Row(
             children: [
-              Expanded(
-                child: Image.network(
-                  'https://images.igdb.com/igdb/image/upload/t_cover_big/${entry.game.cover.imageId}.jpg',
-                  fit: BoxFit.cover,
-                ),
+              Image.network(
+                'https://images.igdb.com/igdb/image/upload/t_cover_big/${entry.game.cover.imageId}.jpg',
+                fit: BoxFit.cover,
               ),
               Padding(padding: EdgeInsets.all(8)),
-              Column(
-                children: [
-                  Center(
-                    child: Text(entry.game.name,
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(entry.game.name,
                         style: Theme.of(context).textTheme.headline3),
-                  ),
-                  Padding(padding: EdgeInsets.all(16)),
-                  Wrap(
-                    spacing: 8.0,
-                    runSpacing: 4.0,
-                    children: [
-                      FilterChip(label: Text('4X'), onSelected: (selected) {}),
-                      FilterChip(
-                          label: Text('Strategy'), onSelected: (selected) {}),
-                      FilterChip(
-                          label: Text('Amplitude'), onSelected: (selected) {}),
-                      FilterChip(
-                          label: Text('Turn-Based Strategy'),
-                          onSelected: (selected) {}),
-                    ],
-                  ),
-                ],
+                    Padding(padding: EdgeInsets.all(16)),
+                    Wrap(
+                      spacing: 8.0,
+                      runSpacing: 4.0,
+                      children: [
+                        FilterChip(
+                            label: Text('4X'), onSelected: (selected) {}),
+                        FilterChip(
+                            label: Text('Strategy'), onSelected: (selected) {}),
+                        FilterChip(
+                            label: Text('Amplitude'),
+                            onSelected: (selected) {}),
+                        FilterChip(
+                            label: Text('Turn-Based Strategy'),
+                            onSelected: (selected) {}),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
@@ -97,7 +97,10 @@ class GameScreen extends StatelessWidget {
             mainAxisSpacing: 16,
             crossAxisSpacing: 16,
             padding: const EdgeInsets.all(16),
-            childAspectRatio: 1.6,
+            childAspectRatio: entry.game.screenshots.isNotEmpty
+                ? entry.game.screenshots[0].width /
+                    entry.game.screenshots[0].height
+                : 1,
             children: [
               for (final screenshot in entry.game.screenshots)
                 GridTile(
