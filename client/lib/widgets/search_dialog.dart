62998be7d04236446f7a6bac0eaf66eb993fee3d
bourdenas <bourdenas@gmail.com>
2021-05-27 13:43:07 +0200

    Expand global search space to tags, collections and companies.

--- client/lib/widgets/search_dialog.dart
+++ client/lib/widgets/search_dialog.dart
@@ -1,4 +1,8 @@
+import 'dart:js';
+
+import 'package:espy/modules/models/game_details_model.dart';
 import 'package:espy/modules/models/game_library_model.dart';
+import 'package:espy/proto/igdbapi.pb.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -25,28 +29,55 @@ class SearchDialog extends StatelessWidget {
                   if (textEditingValue.text == '') {
                     return const [];
                   }
-                  return context
-                      .read<GameLibraryModel>()
-                      .library
-                      .entry
-                      .where((entry) => entry.game.name
-                          .toLowerCase()
-                          .contains(textEditingValue.text.toLowerCase()))
-                      .map((entry) => entry.game.name);
+                  return [
+                    ...context
+                        .read<GameDetailsModel>()
+                        .tags
+                        .where((tag) => tag
+                            .toLowerCase()
+                            .contains(textEditingValue.text.toLowerCase()))
+                        .take(4),
+                    ...context
+                        .read<GameDetailsModel>()
+                        .collections
+                        .where((collection) => collection.name
+                            .toLowerCase()
+                            .contains(textEditingValue.text.toLowerCase()))
+                        .map((collection) => collection.name)
+                        .take(4),
+                    ...context
+                        .read<GameDetailsModel>()
+                        .companies
+                        .where((company) => company.name
+                            .toLowerCase()
+                            .contains(textEditingValue.text.toLowerCase()))
+                        .map((company) => company.name)
+                        .take(4),
+                    ...context
+                        .read<GameLibraryModel>()
+                        .library
+                        .entry
+                        .where((entry) => entry.game.name
+                            .toLowerCase()
+                            .contains(textEditingValue.text.toLowerCase()))
+                        .map((entry) => entry.game.name)
+                        .take(4),
+                  ];
                 },
                 fieldViewBuilder: (context, textEditingController, focusNode,
                     onFieldSubmitted) {
                   return TextFormField(
-                      controller: textEditingController,
-                      focusNode: focusNode,
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.search),
-                        hintText: 'Search...',
-                      ),
-                      onFieldSubmitted: (String value) {
-                        onFieldSubmitted();
-                      });
+                    controller: textEditingController,
+                    focusNode: focusNode,
+                    autofocus: true,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      hintText: 'Search...',
+                    ),
+                    onFieldSubmitted: (String value) {
+                      onFieldSubmitted();
+                    },
+                  );
                 },
                 onSelected: (selection) => print(selection),
               ),
