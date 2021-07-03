c1f87a4d1eef009e4f6211297da6a7cd4fa4841d
bourdenas <bourdenas@gmail.com>
2021-07-03 15:28:46 +0200

    Make search results richer than just text.

--- client/lib/widgets/search_dialog.dart
+++ client/lib/widgets/search_dialog.dart
@@ -6,6 +6,13 @@ import 'package:espy/proto/igdbapi.pb.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+class _SearchOption {
+  final String text;
+  final IconData icon;
+
+  const _SearchOption(this.text, this.icon);
+}
+
 class SearchDialog extends StatelessWidget {
   static Future<void> show(BuildContext context) async {
     showDialog(
@@ -24,35 +31,12 @@ class SearchDialog extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Container(
               width: 400,
-              child: Autocomplete<String>(
+              child: Autocomplete<_SearchOption>(
                 optionsBuilder: (textEditingValue) {
                   if (textEditingValue.text == '') {
                     return const [];
                   }
                   return [
-                    ...context
-                        .read<GameDetailsModel>()
-                        .tags
-                        .where((tag) => tag
-                            .toLowerCase()
-                            .contains(textEditingValue.text.toLowerCase()))
-                        .take(4),
-                    ...context
-                        .read<GameDetailsModel>()
-                        .collections
-                        .where((collection) => collection.name
-                            .toLowerCase()
-                            .contains(textEditingValue.text.toLowerCase()))
-                        .map((collection) => collection.name)
-                        .take(4),
-                    ...context
-                        .read<GameDetailsModel>()
-                        .companies
-                        .where((company) => company.name
-                            .toLowerCase()
-                            .contains(textEditingValue.text.toLowerCase()))
-                        .map((company) => company.name)
-                        .take(4),
                     ...context
                         .read<GameLibraryModel>()
                         .library
@@ -61,12 +45,64 @@ class SearchDialog extends StatelessWidget {
                             .toLowerCase()
                             .contains(textEditingValue.text.toLowerCase()))
                         .map((entry) => entry.game.name)
-                        .take(4),
+                        .take(4)
+                        .map((entry) => _SearchOption(entry, Icons.games)),
+                    ...context
+                        .read<GameDetailsModel>()
+                        .tags
+                        .where((tag) => tag
+                            .toLowerCase()
+                            .contains(textEditingValue.text.toLowerCase()))
+                        .take(4)
+                        .map((tag) => _SearchOption(tag, Icons.tag)),
+                    ...context
+                        .read<GameDetailsModel>()
+                        .collections
+                        .where((collection) => collection.name
+                            .toLowerCase()
+                            .contains(textEditingValue.text.toLowerCase()))
+                        .map((collection) => collection.name)
+                        .take(4)
+                        .map((collection) => _SearchOption(
+                            collection, Icons.branding_watermark)),
+                    ...context
+                        .read<GameDetailsModel>()
+                        .companies
+                        .where((company) => company.name
+                            .toLowerCase()
+                            .contains(textEditingValue.text.toLowerCase()))
+                        .map((company) => company.name)
+                        .take(4)
+                        .map((company) =>
+                            _SearchOption(company, Icons.business)),
                   ];
+                },
+                displayStringForOption: (option) {
+                  return option.text;
+                },
+                optionsViewBuilder: (context, onSelected, options) {
+                  return Material(
+                    elevation: 4.0,
+                    child: Container(
+                      width: 300,
+                      child: ListView(
+                        padding: EdgeInsets.zero,
+                        shrinkWrap: true,
+                        children: [
+                          for (final option in options)
+                            ListTile(
+                              leading: Icon(option.icon),
+                              title: Text(option.text),
+                              onTap: () => onSelected(option),
+                            ),
+                        ],
+                      ),
+                    ),
+                  );
                 },
                 fieldViewBuilder: (context, textEditingController, focusNode,
                     onFieldSubmitted) {
-                  return TextFormField(
+                  return TextField(
                     controller: textEditingController,
                     focusNode: focusNode,
                     autofocus: true,
@@ -74,12 +110,9 @@ class SearchDialog extends StatelessWidget {
                       prefixIcon: Icon(Icons.search),
                       hintText: 'Search...',
                     ),
-                    onFieldSubmitted: (String value) {
-                      onFieldSubmitted();
-                    },
                   );
                 },
-                onSelected: (selection) => print(selection),
+                onSelected: (selection) => print(selection.text),
               ),
             ),
           ),
