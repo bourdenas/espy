94350284b718f0697698ed8c5ef41798d41c68a4
bourdenas <bourdenas@gmail.com>
2021-08-02 20:48:10 +0200

    Switch unmatched game view to use Firestore instead of previous model.

--- client/lib/widgets/library/unmatched_view.dart
+++ client/lib/widgets/library/unmatched_view.dart
@@ -1,29 +1,52 @@
-import 'package:espy/modules/models/unmatched_entries_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:espy/modules/models/user_model.dart';
+import 'package:espy/proto/library.pb.dart';
 import 'package:espy/widgets/dialogs/store_entry_edit_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class UnmatchedView extends StatelessWidget {
+class UnmatchedView extends StatefulWidget {
+  @override
+  _UnmatchedViewState createState() => _UnmatchedViewState();
+}
+
+class _UnmatchedViewState extends State<UnmatchedView> {
   @override
   Widget build(BuildContext context) {
     return Scrollbar(
-      child: ListView(
-        restorationId: 'list_view_unmatched_game_entries_offset',
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        children: context
-            .watch<UnmatchedEntriesModel>()
-            .entries
-            .map(
-              (storeEntry) => ListTile(
+      child: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .doc(context.watch<UserModel>().user.uid)
+            .collection('unknown')
+            .limit(5)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Failed to reach remote server.');
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text("Loading");
+          }
+
+          return ListView(
+            restorationId: 'list_view_unmatched_game_entries_offset',
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+              final item = document.data() as Map<String, dynamic>;
+              return ListTile(
                 title: Row(children: [
-                  Text(storeEntry.title),
+                  Text(item['title']),
                 ]),
-                subtitle: Text('${storeEntry.store}'),
-                onTap: () async =>
-                    await StoreEntryEditDialog.show(context, storeEntry),
-              ),
-            )
-            .toList(),
+                subtitle: Text(item['storefront_name']),
+                onTap: () async => await StoreEntryEditDialog.show(
+                    context,
+                    StoreEntry(
+                      title: item['title'],
+                    )),
+              );
+            }).toList(),
+          );
+        },
       ),
     );
   }
