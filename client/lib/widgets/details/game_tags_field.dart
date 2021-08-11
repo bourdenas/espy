bccc2ea8ae58c9b0909e06878194f1c347d0ee9b
bourdenas <bourdenas@gmail.com>
2021-08-11 16:52:21 +0200

    Replace GameTagsTextField widget with GameTagsField.
    New widget uses AutocompleteField widget for suggestions and allows for suggestion navigation.
    
    Known issues:
     - suggestion navigation is buggy
     - cannot handle substring tags for now

---
+++ client/lib/widgets/details/game_tags_field.dart
@@ -0,0 +1,50 @@
+import 'package:espy/modules/documents/library_entry.dart';
+import 'package:espy/modules/models/game_library_model.dart';
+import 'package:espy/modules/models/game_tags_model.dart';
+import 'package:espy/widgets/autocomplete_field.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class GameTagsField extends StatelessWidget {
+  final LibraryEntry entry;
+
+  const GameTagsField(this.entry, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AutocompleteField(
+      width: 200,
+      hintText: 'Tag...',
+      icon: Icon(Icons.tag),
+      createSuggestions: (text) {
+        final prefix = text.toLowerCase();
+        return [
+          ...context
+              .read<GameTagsIndex>()
+              .tags
+              .where((tag) => tag.toLowerCase().startsWith(prefix))
+              .take(3)
+              .map(
+                (tag) => Suggestion(
+                  text: tag,
+                  onTap: () => _addTag(context, tag),
+                ),
+              ),
+        ];
+      },
+      onSubmit: (text, suggestion) => _addTag(
+        context,
+        suggestion?.text ?? text,
+      ),
+    );
+  }
+
+  void _addTag(BuildContext context, String tag) {
+    if (tag.isEmpty) return;
+
+    // NB: I don't get it why just "entry.userData.tags.add(tag);" fails and I
+    // need to clone GameDetails to edit it.
+    entry.userData = GameUserData(tags: entry.userData.tags + [tag]);
+    context.read<GameLibraryModel>().postDetails(entry);
+  }
+}
