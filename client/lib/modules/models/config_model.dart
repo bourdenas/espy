917a37a9ed4d54a5815211cc96e8f8967d973e6d
bourdenas <bourdenas@gmail.com>
2021-08-18 16:20:40 +0200

    Fix background colour.
    Need to make proper theme.

--- client/lib/modules/models/config_model.dart
+++ client/lib/modules/models/config_model.dart
@@ -11,7 +11,7 @@ class AppConfig extends ChangeNotifier {
 
   get theme => ThemeData(
         brightness: Brightness.dark,
-        primaryColor: foregroundColour,
+        primaryColor: backgrounColour,
         backgroundColor: backgrounColour,
       );
 
