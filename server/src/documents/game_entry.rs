7b5cfeed43396c466fb074ca6f6b889e93d6927b
bourdenas <bourdenas@gmail.com>
2022-03-16 12:31:30 +0100

    Add v2 versions of GameEntry and LibraryEntry documents.

--- server/src/documents/game_entry.rs
+++ server/src/documents/game_entry.rs
@@ -4,6 +4,64 @@ use serde::{Deserialize, Serialize};
 /// Document type under 'users/{user_id}/games' that represents a game entry in
 /// IGDB.
 #[derive(Serialize, Deserialize, Default, Debug)]
+pub struct GameEntryV2 {
+    pub id: u64,
+    pub name: String,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "String::is_empty")]
+    pub summary: String,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "String::is_empty")]
+    pub storyline: String,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub release_date: Option<i64>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub expansions: Vec<GameEntryV2>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub remasters: Vec<GameEntryV2>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub versions: Vec<u64>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub parent: Option<u64>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub collections: Vec<Annotation>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub companies: Vec<Annotation>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub cover: Option<Image>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub screenshots: Vec<Image>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub artwork: Vec<Image>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub websites: Vec<Website>,
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
 pub struct GameEntry {
     pub id: u64,
     pub name: String,
@@ -46,4 +104,27 @@ pub struct Image {
     pub image_id: String,
     pub height: i32,
     pub width: i32,
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct Website {
+    pub url: String,
+    pub authority: WebsiteAuthority,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum WebsiteAuthority {
+    Null = 0,
+    Official = 1,
+    Wikipedia = 2,
+    Gog = 3,
+    Steam = 4,
+    Egs = 5,
+    Youtube = 6,
+}
+
+impl Default for WebsiteAuthority {
+    fn default() -> Self {
+        WebsiteAuthority::Null
+    }
 }
--- server/src/documents/library_entry.rs
+++ server/src/documents/library_entry.rs
@@ -5,6 +5,40 @@ use serde::{Deserialize, Serialize};
 /// Document type under 'users/{user_id}/library/{game_id}' that represents a
 /// game entry in user's library that has been matched with an IGDB entry.
 #[derive(Serialize, Deserialize, Default, Debug)]
+pub struct LibraryEntryV2 {
+    pub id: u64,
+    pub name: String,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub cover: Option<String>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub release_date: Option<i64>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub collections: Vec<Annotation>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub companies: Vec<Annotation>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub store_entry: Vec<StoreEntry>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub owned_versions: Vec<LibraryEntryV2>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub user_data: Option<GameUserData>,
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
 pub struct LibraryEntry {
     pub id: u64,
     pub name: String,
