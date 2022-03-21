30d38b4f2ad497492801f682f37708637805da83
bourdenas <bourdenas@gmail.com>
2022-03-21 22:30:45 +0100

    Fix the search util after the codebase refactoring.

--- server/src/search_igdb.rs
+++ server/src/search_igdb.rs
@@ -1,4 +1,5 @@
 use clap::Clap;
+use documents::GameEntry;
 use espy_server::*;
 
 /// IGDB search utility.
@@ -7,6 +8,12 @@ struct Opts {
     /// Espy user name for managing a game library.
     #[clap(short, long, default_value = "")]
     search: String,
+
+    #[clap(long)]
+    expand: bool,
+
+    #[clap(long)]
+    examine: bool,
 
     /// JSON file that contains application keys for espy service.
     #[clap(long, default_value = "keys.json")]
@@ -22,8 +29,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let mut igdb = api::IgdbApi::new(&keys.igdb.client_id, &keys.igdb.secret);
     igdb.connect().await?;
 
-    let result = igdb.search_by_title(&opts.search).await?;
-    println!("{:#?}", result.games[0]);
+    let mut result = igdb.search_by_title(&opts.search).await?;
+    println!("Found {} candidates.", result.games.len());
+    for game in &result.games {
+        println!("'{}'", &game.name);
+    }
+    if opts.examine && !result.games.is_empty() {
+        let game = igdb.get_game_by_id(result.games[0].id).await?.unwrap();
+        println!("{:#?}", game);
+    }
+    if opts.expand && !result.games.is_empty() {
+        let game_entry = GameEntry::new(std::mem::take(&mut result.games[0]));
+        let game = igdb
+            .get_game_by_id(match game_entry.parent {
+                Some(parent_id) => parent_id,
+                None => game_entry.id,
+            })
+            .await?
+            .unwrap();
+        println!("{:#?}", GameEntry::new(game));
+    }
 
     Ok(())
 }
