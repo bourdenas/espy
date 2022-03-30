375dd8536fb179816e9a2b2cf2bf4e3585a4784f
bourdenas <bourdenas@gmail.com>
2022-03-30 10:20:43 +0200

    Add external website links in the game details page.

--- client/lib/pages/details/game_details_content.dart
+++ client/lib/pages/details/game_details_content.dart
@@ -167,64 +167,98 @@ class GameDetailsContent extends StatelessWidget {
     );
   }
 
-  Row actionBar(BuildContext context) {
-    return Row(
+  Widget actionBar(BuildContext context) {
+    return Column(
       children: [
-        Container(
-          padding: EdgeInsets.symmetric(
-            vertical: 2.0,
-            horizontal: 8.0,
-          ),
-          decoration: BoxDecoration(
-            color: Colors.grey[800],
-            borderRadius: BorderRadius.circular(4.0),
-          ),
-          child: Text(
-            '${DateTime.fromMillisecondsSinceEpoch(gameEntry.releaseDate * 1000).year}',
-            style: TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-        ),
-        SizedBox(width: 16.0),
         Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            IconButton(
-              onPressed: () => context.read<AppConfigModel>().isMobile(context)
-                  ? Navigator.pushNamed(context, '/edit',
-                      arguments: '${libraryEntry.id}')
-                  : EditEntryDialog.show(context, libraryEntry),
-              icon: Icon(
-                Icons.edit,
-                size: 24.0,
+            actionButtons(context),
+            SizedBox(width: 16.0),
+            Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 2.0,
+                horizontal: 8.0,
               ),
-              splashRadius: 20.0,
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.favorite_border_outlined,
-                color: Colors.red,
-                size: 24.0,
+              decoration: BoxDecoration(
+                color: Colors.grey[800],
+                borderRadius: BorderRadius.circular(4.0),
               ),
-              splashRadius: 20.0,
-            ),
-            IconButton(
-              onPressed: () => Navigator.pushNamed(context, '/web',
-                  arguments: '${libraryEntry.id}'),
-              icon: Image.asset('assets/images/gog-128.png'),
-              splashRadius: 20.0,
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Image.asset('assets/images/steam-128.png'),
-              splashRadius: 20.0,
+              child: Text(
+                '${DateTime.fromMillisecondsSinceEpoch(gameEntry.releaseDate * 1000).year}',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
             ),
           ],
         ),
+        linkButtons(context),
       ],
     );
+  }
+
+  Widget actionButtons(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          onPressed: () => context.read<AppConfigModel>().isMobile(context)
+              ? Navigator.pushNamed(context, '/edit',
+                  arguments: '${libraryEntry.id}')
+              : EditEntryDialog.show(context, libraryEntry),
+          icon: Icon(
+            Icons.edit,
+            size: 24.0,
+          ),
+          splashRadius: 20.0,
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.favorite_border_outlined,
+            color: Colors.red,
+            size: 24.0,
+          ),
+          splashRadius: 20.0,
+        ),
+      ],
+    );
+  }
+
+  Widget linkButtons(BuildContext context) {
+    return Row(
+      children: [
+        for (final website in gameEntry.websites)
+          if (website.authority != "Null" && website.authority != "Youtube")
+            IconButton(
+              onPressed: () =>
+                  Navigator.pushNamed(context, '/web', arguments: website.url),
+              icon: websiteIcon(website.authority),
+              splashRadius: 20.0,
+            )
+      ],
+    );
+  }
+
+  Widget websiteIcon(String website) {
+    switch (website) {
+      case "Official":
+        return Icon(Icons.web);
+      case "Wikipedia":
+        return Image.asset('assets/images/wikipedia-128.png');
+      case "Igdb":
+        return Image.asset('assets/images/igdb-128.png');
+      case "Gog":
+        return Image.asset('assets/images/gog-128.png');
+      case "Steam":
+        return Image.asset('assets/images/steam-128.png');
+      case "Egs":
+        return Image.asset('assets/images/egs-128.png');
+      case "Youtube":
+        return Image.asset('assets/images/youtube-128.png');
+    }
+    return Icon(Icons.error);
   }
 
   Widget _fadeShader(Widget child) {
