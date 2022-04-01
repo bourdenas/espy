a66c98b03e4808b8b2a30cb74f72271e8562c849
bourdenas <bourdenas@gmail.com>
2022-04-01 12:45:29 +0200

    Bring back card decoration options for the grid view.

--- client/lib/pages/home/home_page.dart
+++ client/lib/pages/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:espy/modules/dialogs/search/search_dialog.dart';
 import 'package:espy/modules/models/app_config_model.dart';
 import 'package:espy/pages/home/home_content.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   final Function _showMenu;
@@ -63,6 +64,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
 
   AppBar appBar(BuildContext context) {
     final isMobile = AppConfigModel.isMobile(context);
+    final appConfig = context.read<AppConfigModel>();
 
     return AppBar(
       toolbarOpacity: 0.6,
@@ -82,6 +84,27 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
         ),
       ),
       actions: [
+        if (isMobile)
+          IconButton(
+            key: Key('layoutButton'),
+            icon: Icon(_libraryViews[appConfig.libraryLayout.index].iconData),
+            splashRadius: 20.0,
+            onPressed: () {
+              setState(() {
+                appConfig.nextLibraryLayout();
+              });
+            },
+          ),
+        IconButton(
+          key: Key('cardInfoButton'),
+          icon: Icon(_cardViews[appConfig.cardDecoration.index].iconData),
+          splashRadius: 20.0,
+          onPressed: () {
+            setState(() {
+              appConfig.nextCardDecoration();
+            });
+          },
+        ),
         IconButton(
           key: Key('searchButton'),
           icon: Icon(Icons.search),
@@ -89,10 +112,35 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
           onPressed: () => isMobile
               ? Navigator.pushNamed(context, '/search')
               : SearchDialog.show(context),
-        )
+        ),
       ],
       backgroundColor: _colorTween.value,
       elevation: 0.0,
     );
   }
+
+  List<_LibraryView> _libraryViews = const [
+    _LibraryView(LibraryLayout.GRID, Icons.grid_view),
+    _LibraryView(LibraryLayout.LIST, Icons.view_list),
+  ];
+
+  List<_CardsView> _cardViews = const [
+    _CardsView(CardDecoration.EMPTY, Icons.label_off),
+    _CardsView(CardDecoration.INFO, Icons.info),
+    _CardsView(CardDecoration.TAGS, Icons.collections_bookmark),
+  ];
+}
+
+class _LibraryView {
+  final LibraryLayout layout;
+  final IconData iconData;
+
+  const _LibraryView(this.layout, this.iconData);
+}
+
+class _CardsView {
+  final CardDecoration decoration;
+  final IconData iconData;
+
+  const _CardsView(this.decoration, this.iconData);
 }
