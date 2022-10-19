751681cc73677f8311a3398c4cdb0dbfd131588f
bourdenas <bourdenas@gmail.com>
2022-10-19 22:29:11 +0200

    Delete v1 widgets.

--- client/lib/old_widgets/library/library_view.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-abstract class LibraryView extends StatelessWidget {
-  const LibraryView({Key? key}) : super(key: key);
-
-  int visibleEntries(BuildContext context);
-
-  double get scrollThreshold;
-}
