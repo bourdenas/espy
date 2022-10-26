72031942d625ddfe681ef28211a34263e1bc05f5
bourdenas <bourdenas@gmail.com>
2022-10-26 17:24:33 +0200

    Add a ReconReport struct that logs recon success/failure.

--- server/src/library/mod.rs
+++ server/src/library/mod.rs
@@ -1,10 +1,12 @@
 mod library_ops;
 mod manager;
+mod recon_report;
 mod reconciler;
 pub mod search;
 mod steam_data;
 mod user;
 
 pub use manager::LibraryManager;
+pub use recon_report::ReconReport;
 pub use reconciler::Reconciler;
 pub use user::User;
