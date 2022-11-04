5ed20995d9ad44d01473c6135919bde64413b3d7
bourdenas <bourdenas@gmail.com>
2022-11-04 15:06:59 +0100

    Construct a new LibraryEntry from GameEntry in the updated schema.
    LibraryEntry only contains deduped labels for collections and companies. Details stay with GameEntry.

--- server/src/documents/library_entry.rs
+++ server/src/documents/library_entry.rs
@@ -1,7 +1,8 @@
-use super::Annotation;
-use crate::documents::{Collection, Company, GameEntry, StoreEntry};
+use super::{Annotation, CompanyRole};
+use crate::documents::{GameEntry, StoreEntry};
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
-use std::fmt;
+use std::{cmp::Ordering, collections::HashSet, fmt};
 
 /// Document type under 'users/{user_id}/library_v2/{game_id}' that represents a
 /// game entry in user's library that has been matched with an IGDB entry.
@@ -54,8 +55,40 @@ impl LibraryEntry {
                 None => None,
             },
             release_date: game.release_date,
-            collections: game.collections,
-            companies: game.companies,
+
+            collections: game
+                .collections
+                .iter()
+                .map(|collection| collection.name.clone())
+                .collect::<HashSet<_>>()
+                .into_iter()
+                .collect(),
+
+            companies: game
+                .companies
+                .iter()
+                .filter(|company| match company.role {
+                    CompanyRole::Developer => true,
+                    CompanyRole::Publisher => true,
+                    _ => false,
+                })
+                .sorted_by(|l, r| match l.role {
+                    CompanyRole::Developer => match r.role {
+                        CompanyRole::Developer => Ordering::Equal,
+                        _ => Ordering::Less,
+                    },
+                    CompanyRole::Publisher => match r.role {
+                        CompanyRole::Developer => Ordering::Greater,
+                        CompanyRole::Publisher => Ordering::Equal,
+                        _ => Ordering::Less,
+                    },
+                    _ => Ordering::Less,
+                })
+                .map(|company| company.name.clone())
+                .collect::<HashSet<_>>()
+                .into_iter()
+                .collect(),
+
             store_entries,
             owned_versions,
             user_data,
@@ -91,11 +124,11 @@ pub struct LibraryEntry {
 
     #[serde(default)]
     #[serde(skip_serializing_if = "Vec::is_empty")]
-    pub collections: Vec<Collection>,
+    pub collections: Vec<String>,
 
     #[serde(default)]
     #[serde(skip_serializing_if = "Vec::is_empty")]
-    pub companies: Vec<Company>,
+    pub companies: Vec<String>,
 
     #[serde(default)]
     #[serde(skip_serializing_if = "Vec::is_empty")]
