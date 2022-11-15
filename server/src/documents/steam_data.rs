65ee6d0f64e3459552d6ac3a48a83dabc8987cf0
bourdenas <bourdenas@gmail.com>
2022-11-15 21:16:32 +0100

    Store developer/publisher and genres from Steam data.

--- server/src/documents/steam_data.rs
+++ server/src/documents/steam_data.rs
@@ -18,7 +18,19 @@ pub struct SteamData {
 
     #[serde(default)]
     #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub developers: Vec<String>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub publishers: Vec<String>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
     pub dlc: Vec<u64>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub genres: Vec<Genre>,
 
     #[serde(default)]
     #[serde(skip_serializing_if = "Vec::is_empty")]
@@ -27,6 +39,12 @@ pub struct SteamData {
     #[serde(default)]
     #[serde(skip_serializing_if = "Vec::is_empty")]
     pub movies: Vec<Movie>,
+}
+
+#[derive(Serialize, Deserialize, Default, Clone, Debug)]
+pub struct Genre {
+    pub id: String,
+    pub description: String,
 }
 
 #[derive(Serialize, Deserialize, Default, Clone, Debug)]
