23ec4ef88e4bfa79fae2d99026a5c30125422b82
bourdenas <bourdenas@gmail.com>
2022-11-19 10:26:53 +0100

    Implement add/remove tags operation on UserTags.

--- server/src/documents/user_tags.rs
+++ server/src/documents/user_tags.rs
@@ -14,5 +14,36 @@ pub struct Tag {
 
     #[serde(default)]
     #[serde(skip_serializing_if = "Vec::is_empty")]
-    pub games: Vec<u64>,
+    pub game_ids: Vec<u64>,
+}
+
+impl UserTags {
+    /// Returns true if `tag` was added on `game_id`, false if it existed already.
+    pub fn add(&mut self, game_id: u64, tag_name: String) -> bool {
+        match self.tags.iter_mut().find(|tag| tag.name == tag_name) {
+            Some(tag) => {
+                if tag.game_ids.contains(&game_id) {
+                    return false;
+                }
+                tag.game_ids.push(game_id)
+            }
+            None => self.tags.push(Tag {
+                name: tag_name,
+                game_ids: vec![game_id],
+            }),
+        }
+        true
+    }
+
+    /// Returns true if `tag` was removed from `game_id`, false if it did not exist.
+    pub fn remove(&mut self, game_id: u64, tag_name: &str) -> bool {
+        let Some(tag) = self.tags.iter_mut().find(|tag| tag.name == tag_name) else {
+            return false;
+        };
+        let Some(index) = tag.game_ids.iter().position(|id| *id == game_id) else {
+            return false;
+        };
+        tag.game_ids.remove(index);
+        true
+    }
 }
