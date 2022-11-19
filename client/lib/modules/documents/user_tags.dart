5500c603410aa3773957b1db28fc58ccfcf6fd8b
bourdenas <bourdenas@gmail.com>
2022-11-19 10:50:17 +0100

    Add new UserTags firestore document.

---
+++ client/lib/modules/documents/user_tags.dart
@@ -0,0 +1,45 @@
+class UserTags {
+  final List<Tag> tags;
+
+  UserTags({
+    required this.tags,
+  });
+
+  UserTags.fromJson(Map<String, dynamic> json)
+      : this(
+          tags: [
+            for (final tag in json['tags'] ?? []) Tag.fromJson(tag),
+          ],
+        );
+
+  Map<String, dynamic> toJson() {
+    return {
+      'tags': tags,
+    };
+  }
+}
+
+class Tag {
+  final String name;
+  final List<int> gameIds;
+
+  Tag({
+    required this.name,
+    required this.gameIds,
+  });
+
+  Tag.fromJson(Map<String, dynamic> json)
+      : this(
+          name: json['name']!,
+          gameIds: [
+            for (int id in json['game_ids'] ?? []) id,
+          ],
+        );
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'game_ids': gameIds,
+    };
+  }
+}
