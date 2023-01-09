3645dc21c38ce99a96ce4f1398505c94671779a6
bourdenas <bourdenas@gmail.com>
2023-01-09 20:38:20 +0100

    Add GameDigest document and make it part of LibraryEntry.
    Add LegacyLibraryEntry for the transition.

--- server/src/documents/library_entry.rs
+++ server/src/documents/library_entry.rs
@@ -1,4 +1,4 @@
-use super::CompanyRole;
+use super::{CompanyRole, GameDigest};
 use crate::documents::{GameEntry, StoreEntry};
 use itertools::Itertools;
 use serde::{Deserialize, Serialize};
@@ -8,6 +8,20 @@ use std::{cmp::Ordering, collections::HashSet, fmt};
 /// game entry in user's library that has been matched with an IGDB entry.
 #[derive(Serialize, Deserialize, Default, Debug)]
 pub struct LibraryEntry {
+    pub id: u64,
+    pub digest: GameDigest,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub store_entries: Vec<StoreEntry>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub owned_versions: Vec<u64>,
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct LegacyLibraryEntry {
     pub id: u64,
     pub name: String,
 
@@ -40,46 +54,48 @@ impl LibraryEntry {
     pub fn new(game: GameEntry, store_entries: Vec<StoreEntry>, owned_versions: Vec<u64>) -> Self {
         LibraryEntry {
             id: game.id,
-            name: game.name,
-            cover: match game.cover {
-                Some(cover) => Some(cover.image_id),
-                None => None,
+            digest: GameDigest {
+                name: game.name,
+                cover: match game.cover {
+                    Some(cover) => Some(cover.image_id),
+                    None => None,
+                },
+                release_date: game.release_date,
+                rating: game.igdb_rating,
+
+                collections: game
+                    .collections
+                    .iter()
+                    .map(|collection| collection.name.clone())
+                    .collect::<HashSet<_>>()
+                    .into_iter()
+                    .collect(),
+
+                companies: game
+                    .companies
+                    .iter()
+                    .filter(|company| match company.role {
+                        CompanyRole::Developer => true,
+                        CompanyRole::Publisher => true,
+                        _ => false,
+                    })
+                    .sorted_by(|l, r| match l.role {
+                        CompanyRole::Developer => match r.role {
+                            CompanyRole::Developer => Ordering::Equal,
+                            _ => Ordering::Greater,
+                        },
+                        CompanyRole::Publisher => match r.role {
+                            CompanyRole::Developer => Ordering::Less,
+                            CompanyRole::Publisher => Ordering::Equal,
+                            _ => Ordering::Greater,
+                        },
+                        _ => Ordering::Less,
+                    })
+                    .map(|company| company.name.clone())
+                    .collect::<HashSet<_>>()
+                    .into_iter()
+                    .collect(),
             },
-            release_date: game.release_date,
-
-            collections: game
-                .collections
-                .iter()
-                .map(|collection| collection.name.clone())
-                .collect::<HashSet<_>>()
-                .into_iter()
-                .collect(),
-
-            companies: game
-                .companies
-                .iter()
-                .filter(|company| match company.role {
-                    CompanyRole::Developer => true,
-                    CompanyRole::Publisher => true,
-                    _ => false,
-                })
-                .sorted_by(|l, r| match l.role {
-                    CompanyRole::Developer => match r.role {
-                        CompanyRole::Developer => Ordering::Equal,
-                        _ => Ordering::Greater,
-                    },
-                    CompanyRole::Publisher => match r.role {
-                        CompanyRole::Developer => Ordering::Less,
-                        CompanyRole::Publisher => Ordering::Equal,
-                        _ => Ordering::Greater,
-                    },
-                    _ => Ordering::Less,
-                })
-                .map(|company| company.name.clone())
-                .collect::<HashSet<_>>()
-                .into_iter()
-                .collect(),
-
             store_entries,
             owned_versions,
         }
@@ -88,6 +104,6 @@ impl LibraryEntry {
 
 impl fmt::Display for LibraryEntry {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "LibraryEntry({}): '{}'", &self.id, &self.name)
+        write!(f, "LibraryEntry({}): '{}'", &self.id, &self.digest.name)
     }
 }
