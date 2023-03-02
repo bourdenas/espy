1caefe60ff9b82856b4feea57b71bd32c684b632
bourdenas <bourdenas@gmail.com>
2023-03-02 22:37:23 +0100

    Reconnect to firestore every 30min during game entries refresh.
    Firestore connection times-out after a certain time which causes long pipelines to fail. Refresh connection to avoid this error.

--- server/src/utils/refresh_entries.rs
+++ server/src/utils/refresh_entries.rs
@@ -1,7 +1,10 @@
 use clap::Parser;
 use espy_server::{games::Resolver, *};
-use std::sync::{Arc, Mutex};
-use tracing::{error, instrument};
+use std::{
+    sync::{Arc, Mutex},
+    time::{Duration, SystemTime},
+};
+use tracing::{error, info, instrument};
 
 /// Espy util for refreshing IGDB and Steam data for GameEntries.
 #[derive(Parser)]
@@ -31,21 +34,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     Tracing::setup("utils/refresh_entries")?;
 
     let opts: Opts = Opts::parse();
-    let firestore = api::FirestoreApi::from_credentials(&opts.firestore_credentials)
-        .expect("FirestoreApi.from_credentials()");
-
     let keys = util::keys::Keys::from_file(&opts.key_store).unwrap();
+
     let mut igdb = api::IgdbApi::new(&keys.igdb.client_id, &keys.igdb.secret);
     igdb.connect().await?;
+
     let steam = games::SteamDataApi::new();
 
     if let Some(id) = opts.id {
         match opts.delete {
-            false => refresh_game(id, firestore, igdb, steam).await?,
-            true => library::firestore::games::delete(&firestore, id)?,
+            false => refresh_game(id, &opts.firestore_credentials, igdb, steam).await?,
+            true => {
+                let firestore = api::FirestoreApi::from_credentials(&opts.firestore_credentials)
+                    .expect("FirestoreApi.from_credentials()");
+                library::firestore::games::delete(&firestore, id)?
+            }
         }
     } else {
-        refresh_entries(firestore, igdb, steam).await?;
+        refresh_entries(&opts.firestore_credentials, igdb, steam).await?;
     }
 
     Ok(())
@@ -53,33 +59,54 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
 async fn refresh_game(
     id: u64,
-    firestore: api::FirestoreApi,
+    firestore_credentials: &str,
     igdb: api::IgdbApi,
     steam: games::SteamDataApi,
 ) -> Result<(), Status> {
+    let firestore = api::FirestoreApi::from_credentials(firestore_credentials)
+        .expect("FirestoreApi.from_credentials()");
+
     let game = library::firestore::games::read(&firestore, id)?;
-    refresh(vec![game], firestore, igdb, steam).await
+    refresh(vec![game], firestore_credentials, igdb, steam).await
 }
 
-#[instrument(level = "trace", skip(firestore, igdb, steam))]
+#[instrument(level = "trace", skip(firestore_credentials, igdb, steam))]
 async fn refresh_entries(
-    firestore: api::FirestoreApi,
+    firestore_credentials: &str,
     igdb: api::IgdbApi,
     steam: games::SteamDataApi,
 ) -> Result<(), Status> {
+    let firestore = api::FirestoreApi::from_credentials(firestore_credentials)
+        .expect("FirestoreApi.from_credentials()");
+
     let game_entries = library::firestore::games::list(&firestore)?;
-    refresh(game_entries, firestore, igdb, steam).await
+    refresh(game_entries, firestore_credentials, igdb, steam).await
 }
 
 async fn refresh(
     game_entries: Vec<documents::GameEntry>,
-    firestore: api::FirestoreApi,
+    firestore_credentials: &str,
     igdb: api::IgdbApi,
     steam: games::SteamDataApi,
 ) -> Result<(), Status> {
-    let firestore = Arc::new(Mutex::new(firestore));
+    let mut firestore = Arc::new(Mutex::new(
+        api::FirestoreApi::from_credentials(firestore_credentials)
+            .expect("FirestoreApi.from_credentials()"),
+    ));
+    let next_refresh = SystemTime::now()
+        .checked_add(Duration::from_secs(30 * 60))
+        .unwrap();
+
+    info!("Updating {} game entries...", game_entries.len());
 
     for game_entry in game_entries {
+        if next_refresh < SystemTime::now() {
+            firestore = Arc::new(Mutex::new(
+                api::FirestoreApi::from_credentials(firestore_credentials)
+                    .expect("FirestoreApi.from_credentials()"),
+            ));
+        }
+
         if let Err(e) =
             Resolver::resolve(game_entry.id, &igdb, &steam, Arc::clone(&firestore)).await
         {
