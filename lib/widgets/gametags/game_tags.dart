75208b394266e582bc67955748aced2c683449c4
bourdenas <bourdenas@gmail.com>
2023-07-01 17:23:51 +0200

    Show keyword filter chips in the library view.

--- lib/widgets/gametags/game_tags.dart
+++ lib/widgets/gametags/game_tags.dart
@@ -351,6 +351,24 @@ class GameChipsFilter extends StatelessWidget {
           ),
         ),
       ],
+      for (final keyword in filter.keywords) ...[
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: KeywordChip(
+            keyword,
+            onDeleted: () {
+              final filter = context
+                  .read<LibraryFilterModel>()
+                  .filter
+                  .remove(LibraryFilter(keywords: {keyword}));
+              context.pushNamed(
+                'games',
+                queryParameters: filter.params(),
+              );
+            },
+          ),
+        ),
+      ],
       for (final tag in filter.tags) ...[
         Padding(
           padding: const EdgeInsets.all(4.0),
