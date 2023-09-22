1a11872ac7518b58244de857e26b32290f36ecae
bourdenas <bourdenas@gmail.com>
2023-09-22 16:36:36 +0200

    Add chewie player in deps and make video playback work.

--- lib/modules/dialogs/video_dialog.dart
+++ lib/modules/dialogs/video_dialog.dart
@@ -1,3 +1,4 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
@@ -15,20 +16,30 @@ class VideoDialog extends StatefulWidget {
 
 class _VideoDialogState extends State<VideoDialog> {
   late VideoPlayerController _controller;
+  late ChewieController _chewieController;
 
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.videoUrl))
-      ..initialize().then((_) => setState(() {
-            _controller.play();
-          }));
+
+    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.videoUrl));
   }
 
   @override
   void dispose() {
-    super.dispose();
+    _chewieController.dispose();
     _controller.dispose();
+    super.dispose();
+  }
+
+  Future<ChewieController> init() async {
+    await _controller.initialize();
+    _chewieController = ChewieController(
+      videoPlayerController: _controller,
+      autoPlay: true,
+    );
+
+    return _chewieController;
   }
 
   @override
@@ -36,15 +47,24 @@ class _VideoDialogState extends State<VideoDialog> {
     return AlertDialog(
       backgroundColor: Colors.transparent,
       insetPadding: const EdgeInsets.all(2),
-      title: _controller.value.isInitialized
-          ? AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              child: VideoPlayer(_controller),
-            )
-          : const SizedBox(
-              width: 800,
-              height: 600,
-            ),
+      title: FutureBuilder<ChewieController>(
+        future: init(),
+        builder:
+            (BuildContext context, AsyncSnapshot<ChewieController> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.hasData) {
+            return SizedBox(
+              width: 1200,
+              height: 800,
+              child: Chewie(
+                controller: snapshot.data!,
+              ),
+            );
+          }
+
+          return Container();
+        },
+      ),
     );
   }
 }
