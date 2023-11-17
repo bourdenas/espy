72e5cbd6755d7c9dd9adfdd44d65ed9c9bafc553
bourdenas <bourdenas@gmail.com>
2023-11-17 15:02:10 +0100

    Update project after upgrading to flutter 3.16

--- lib/pages/profile/login_page.dart
+++ lib/pages/profile/login_page.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutterfire_ui/auth.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const SignInScreen(
-      providerConfigs: _providerConfigs,
       actions: [],
     );
   }
 }
-
-const _providerConfigs = [
-  // EmailProviderConfiguration(),
-  GoogleProviderConfiguration(
-    clientId:
-        '478783154654-gq2jbr76gn0eggo0i71ak51bu9l3q7q5.apps.googleusercontent.com',
-  ),
-];
