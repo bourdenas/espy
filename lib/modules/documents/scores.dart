d489a97d58f21f9adc14454a0670dbdf8eb5e646
bourdenas <bourdenas@gmail.com>
2024-02-25 21:10:47 +0100

    Add hype field to the Scores document.

--- lib/modules/documents/scores.dart
+++ lib/modules/documents/scores.dart
@@ -1,6 +1,7 @@
 class Scores {
   final int? thumbs;
   final int? popularity;
+  final int? hype;
   final int? metacritic;
   final int? espyScore;
 
@@ -9,6 +10,7 @@ class Scores {
   const Scores({
     this.thumbs,
     this.popularity,
+    this.hype,
     this.metacritic,
     this.espyScore,
     this.espyTier,
@@ -18,6 +20,7 @@ class Scores {
       : this(
           thumbs: json['thumbs'],
           popularity: json['popularity'],
+          hype: json['hype'],
           metacritic: json['metacritic'],
           espyScore: json['espy_score'],
           espyTier: json['espy_tier'],
@@ -27,6 +30,7 @@ class Scores {
     return {
       if (thumbs != null) 'thumbs': thumbs,
       if (popularity != null) 'popularity': popularity,
+      if (hype != null) 'hype': hype,
       if (metacritic != null) 'metacritic': metacritic,
       if (espyScore != null) 'espy_score': espyScore,
       if (espyTier != null) 'espy_tier': espyTier,
@@ -36,6 +40,7 @@ class Scores {
   bool hasDiff(Scores other) {
     return thumbs != other.thumbs ||
         popularity != other.popularity ||
+        hype != other.hype ||
         metacritic != other.metacritic ||
         espyScore != other.espyScore ||
         espyTier != other.espyTier;
