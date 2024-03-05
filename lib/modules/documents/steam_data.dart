d02ff4a1733af4fbd32e5eeaaa248201c795a25b
bourdenas <bourdenas@gmail.com>
2024-03-05 21:53:25 +0100

    Fix steam genre description field name.

--- lib/modules/documents/steam_data.dart
+++ lib/modules/documents/steam_data.dart
@@ -98,23 +98,23 @@ class SteamData {
 
 class Genre {
   final String id;
-  final String name;
+  final String description;
 
   const Genre({
     required this.id,
-    required this.name,
+    required this.description,
   });
 
   Genre.fromJson(Map<String, dynamic> json)
       : this(
           id: json['id'] ?? '',
-          name: json['name'] ?? '',
+          description: json['description'] ?? '',
         );
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
-      'name': name,
+      'description': description,
     };
   }
 }
