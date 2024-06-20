1a130f174b4e8b5b756a8c06ec6229763133f854
bourdenas <bourdenas@gmail.com>
2024-06-20 22:27:36 +0200

    Move library filters to their own module.

--- lib/pages/library/library_page.dart
+++ lib/pages/library/library_page.dart
@@ -2,17 +2,15 @@ import 'package:badges/badges.dart' as badges;
 import 'package:espy/modules/documents/library_entry.dart';
 import 'package:espy/modules/filtering/library_filter.dart';
 import 'package:espy/modules/models/app_config_model.dart';
-import 'package:espy/modules/models/game_tags_model.dart';
 import 'package:espy/modules/models/library_filter_model.dart';
 import 'package:espy/modules/models/library_view_model.dart';
 import 'package:espy/modules/models/user_model.dart';
 import 'package:espy/pages/library/library_entries_view.dart';
-import 'package:espy/widgets/gametags/game_chips.dart';
-import 'package:espy/widgets/sliding_chip.dart';
+import 'package:espy/widgets/filters/categories_sliding_chip.dart';
+import 'package:espy/widgets/filters/genres_sliding_chip.dart';
 import 'package:espy/widgets/gametags/game_chips_filter_bar.dart';
 import 'package:espy/widgets/tiles/tile_shelve.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 import 'package:provider/provider.dart';
 
 class LibraryPage extends StatelessWidget {
@@ -85,187 +83,17 @@ class LibraryPage extends StatelessWidget {
           children: [
             GameChipsFilterBar(filter),
             const SizedBox(width: 8),
-            SlidingChip(
-              label: 'Categories',
-              color: Theme.of(context).colorScheme.onSecondaryContainer,
-              backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
-              expansion: const GameCategoryFilter(),
-            ),
+            const GameCategoriesSlidingChip(),
             const SizedBox(width: 8.0),
-            SlidingChip(
-              label: 'Genres',
-              // color: Theme.of(context).colorScheme.onSecondaryContainer,
-              color: GenreGroupChip.color,
-              expansion: const GameGenreGroupFilter(),
-            ),
+            const GameGenresSlidingChip(),
             const SizedBox(width: 8.0),
             if (context.watch<UserModel>().isSignedIn)
-              LibraryChoice('External', appConfig.showExternal),
+              CategoryFilterChip('External', appConfig.showExternal),
           ],
         ),
       ),
       backgroundColor: Colors.black.withOpacity(0.6),
       elevation: 0.0,
-    );
-  }
-}
-
-class GameGenreGroupFilter extends StatefulWidget {
-  const GameGenreGroupFilter({super.key});
-
-  @override
-  State<GameGenreGroupFilter> createState() => _GameGenreGroupFilterState();
-}
-
-class _GameGenreGroupFilterState extends State<GameGenreGroupFilter> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  String? activeGroup;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      // children: buildGenreGroups(context),
-      children: (activeGroup == null)
-          ? buildGenreGroups(context)
-          : buildEspyGenre(context, activeGroup!),
-    );
-  }
-
-  List<Widget> buildGenreGroups(BuildContext context) {
-    return [
-      for (final group in context.read<GameTagsModel>().genreGroups)
-        if (activeGroup == null || activeGroup == group) ...[
-          EspyFilterChip(
-              label: group,
-              color: GenreGroupChip.color,
-              onClick: () {
-                setState(() => activeGroup = group);
-              }),
-          const SizedBox(width: 8),
-        ],
-    ];
-  }
-
-  List<Widget> buildEspyGenre(BuildContext context, String genreGroup) {
-    return [
-      EspyFilterChip(
-          label: genreGroup,
-          backgroundColor: GenreGroupChip.color,
-          open: true,
-          onClick: () {
-            setState(() => activeGroup = null);
-          }),
-      const SizedBox(width: 4),
-      for (final genre
-          in context.read<GameTagsModel>().espyGenreTags(genreGroup) ?? []) ...[
-        EspyGenreTagChip(
-          genre,
-          onPressed: () {},
-          filled: false,
-        ),
-        const SizedBox(width: 8),
-      ],
-    ];
-  }
-}
-
-class EspyFilterChip extends StatelessWidget {
-  const EspyFilterChip({
-    super.key,
-    required this.label,
-    this.color,
-    this.backgroundColor,
-    this.openIcon = Icons.keyboard_arrow_right,
-    this.closeIcon = Icons.keyboard_arrow_left,
-    required this.onClick,
-    this.open = false,
-  });
-
-  final String label;
-  final Color? color;
-  final Color? backgroundColor;
-  final IconData openIcon;
-  final IconData closeIcon;
-  final void Function() onClick;
-  final bool open;
-
-  @override
-  Widget build(BuildContext context) {
-    return InputChip(
-      label: Row(
-        children: [
-          Icon(
-            open ? closeIcon : openIcon,
-            color: color,
-          ),
-          const SizedBox(width: 4),
-          Text(
-            label,
-            style:
-                Theme.of(context).textTheme.bodyMedium!.copyWith(color: color),
-          ),
-        ],
-      ),
-      backgroundColor: backgroundColor,
-      onPressed: onClick,
-    );
-  }
-}
-
-class GameCategoryFilter extends StatelessWidget {
-  const GameCategoryFilter({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final appConfig = context.watch<AppConfigModel>();
-    return Row(
-      children: [
-        LibraryChoice('Main Games', appConfig.showMains),
-        const SizedBox(width: 8),
-        LibraryChoice('Expansions', appConfig.showExpansions),
-        const SizedBox(width: 8),
-        LibraryChoice('DLCs', appConfig.showDlcs),
-        const SizedBox(width: 8),
-        LibraryChoice('Versions', appConfig.showVersions),
-        const SizedBox(width: 8),
-        LibraryChoice('Bundles', appConfig.showBundles),
-        const SizedBox(width: 8),
-      ],
-    );
-  }
-}
-
-class LibraryChoice extends StatelessWidget {
-  const LibraryChoice(
-    this.label,
-    this.option, {
-    super.key,
-  });
-
-  final String label;
-  final BoolOption option;
-
-  @override
-  Widget build(BuildContext context) {
-    final appConfig = context.read<AppConfigModel>();
-    return GestureDetector(
-      onSecondaryTap: () {
-        appConfig.showMains.value = false;
-        appConfig.showExpansions.value = false;
-        appConfig.showDlcs.value = false;
-        appConfig.showVersions.value = false;
-        appConfig.showBundles.value = false;
-        option.value = true;
-      },
-      child: ChoiceChip(
-        label: Text(label),
-        selected: option.value,
-        onSelected: (selected) => option.value = selected,
-      ),
     );
   }
 }
---
+++ lib/widgets/filters/genres_sliding_chip.dart
@@ -0,0 +1,127 @@
+import 'package:espy/modules/models/game_tags_model.dart';
+import 'package:espy/widgets/filters/sliding_chip.dart';
+import 'package:espy/widgets/gametags/game_chips.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class GameGenresSlidingChip extends StatelessWidget {
+  const GameGenresSlidingChip({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SlidingChip(
+      label: 'Genres',
+      // color: Theme.of(context).colorScheme.onSecondaryContainer,
+      color: GenreGroupChip.color,
+      expansion: const GameGenreGroupFilter(),
+    );
+  }
+}
+
+class GameGenreGroupFilter extends StatefulWidget {
+  const GameGenreGroupFilter({super.key});
+
+  @override
+  State<GameGenreGroupFilter> createState() => _GameGenreGroupFilterState();
+}
+
+class _GameGenreGroupFilterState extends State<GameGenreGroupFilter> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  String? activeGroup;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      // children: buildGenreGroups(context),
+      children: (activeGroup == null)
+          ? buildGenreGroups(context)
+          : buildEspyGenre(context, activeGroup!),
+    );
+  }
+
+  List<Widget> buildGenreGroups(BuildContext context) {
+    return [
+      for (final group in context.read<GameTagsModel>().genreGroups)
+        if (activeGroup == null || activeGroup == group) ...[
+          GenreFilterChip(
+              label: group,
+              color: GenreGroupChip.color,
+              onClick: () {
+                setState(() => activeGroup = group);
+              }),
+          const SizedBox(width: 8),
+        ],
+    ];
+  }
+
+  List<Widget> buildEspyGenre(BuildContext context, String genreGroup) {
+    return [
+      GenreFilterChip(
+          label: genreGroup,
+          backgroundColor: GenreGroupChip.color,
+          open: true,
+          onClick: () {
+            setState(() => activeGroup = null);
+          }),
+      const SizedBox(width: 4),
+      for (final genre
+          in context.read<GameTagsModel>().espyGenreTags(genreGroup) ?? []) ...[
+        EspyGenreTagChip(
+          genre,
+          onPressed: () {},
+          filled: false,
+        ),
+        const SizedBox(width: 8),
+      ],
+    ];
+  }
+}
+
+class GenreFilterChip extends StatelessWidget {
+  const GenreFilterChip({
+    super.key,
+    required this.label,
+    this.color,
+    this.backgroundColor,
+    this.openIcon = Icons.keyboard_arrow_right,
+    this.closeIcon = Icons.keyboard_arrow_left,
+    required this.onClick,
+    this.open = false,
+  });
+
+  final String label;
+  final Color? color;
+  final Color? backgroundColor;
+  final IconData openIcon;
+  final IconData closeIcon;
+  final void Function() onClick;
+  final bool open;
+
+  @override
+  Widget build(BuildContext context) {
+    return InputChip(
+      label: Row(
+        children: [
+          Icon(
+            open ? closeIcon : openIcon,
+            color: color,
+          ),
+          const SizedBox(width: 4),
+          Text(
+            label,
+            style:
+                Theme.of(context).textTheme.bodyMedium!.copyWith(color: color),
+          ),
+        ],
+      ),
+      backgroundColor: backgroundColor,
+      onPressed: onClick,
+    );
+  }
+}
