ae212f4af78d7436970b94bd4ab7f0af397a718b
bourdenas <bourdenas@gmail.com>
2024-10-24 21:33:01 +0200

    Fix IgdbCompany doc encoding/decoding.

--- lib/modules/documents/igdb_company.dart
+++ lib/modules/documents/igdb_company.dart
@@ -27,7 +27,9 @@ class IgdbCompany {
           name: json['name']!,
           slug: json['slug']!,
           description: json['description'],
-          logo: json['logo'],
+          logo: json.containsKey('logo')
+              ? GameImage.fromJson(json['logo'])
+              : null,
           developed: [
             for (final game in json['developed'] ?? [])
               GameDigest.fromJson(game),
@@ -44,7 +46,7 @@ class IgdbCompany {
       'name': name,
       'slug': slug,
       if (description != null) 'description': description,
-      if (logo != null) 'logo': logo,
+      if (logo != null) 'logo': logo?.toJson(),
       if (developed.isNotEmpty)
         'developed': [
           for (final game in developed) game.toJson(),
