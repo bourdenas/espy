b6c6f1adb58a76082ba0e83713bb5b0bfd9e5b5c
bourdenas <bourdenas@gmail.com>
2024-10-25 15:23:32 +0200

    Make subsidiaries clickable which filters games of each one.

--- lib/pages/company/company_page.dart
+++ lib/pages/company/company_page.dart
@@ -1,14 +1,10 @@
-import 'package:badges/badges.dart' as badges;
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:espy/constants/urls.dart';
 import 'package:espy/modules/documents/igdb_company.dart';
 import 'package:espy/modules/documents/library_entry.dart';
-import 'package:espy/modules/filtering/library_filter.dart';
 import 'package:espy/modules/models/app_config_model.dart';
 import 'package:espy/modules/models/backend_api.dart';
 import 'package:espy/modules/models/library_view_model.dart';
-import 'package:espy/pages/library/library_entries_view.dart';
-import 'package:espy/pages/library/library_page.dart';
 import 'package:espy/pages/library/library_stats.dart';
 import 'package:espy/widgets/shelve.dart';
 import 'package:espy/widgets/tiles/tile_shelve.dart';
@@ -35,30 +31,37 @@ class CompanyPage extends StatelessWidget {
   }
 }
 
-class CompanyContent extends StatelessWidget {
+class CompanyContent extends StatefulWidget {
   const CompanyContent(this.companyDocs, {super.key});
 
   final List<IgdbCompany> companyDocs;
 
   @override
+  State<CompanyContent> createState() => _CompanyContentState();
+}
+
+class _CompanyContentState extends State<CompanyContent> {
+  IgdbCompany? selectedCompany;
+
+  @override
   Widget build(BuildContext context) {
     final appConfig = context.watch<AppConfigModel>();
 
-    companyDocs.sort((l, r) => -(l.developed.length + l.published.length)
+    widget.companyDocs.sort((l, r) => -(l.developed.length + l.published.length)
         .compareTo(r.developed.length + r.published.length));
 
-    final logoUrl = companyDocs.first.logo != null
-        ? '${Urls.imageProvider}/t_logo_med/${companyDocs.first.logo!.imageId}.png'
+    final logoUrl = widget.companyDocs.first.logo != null
+        ? '${Urls.imageProvider}/t_logo_med/${widget.companyDocs.first.logo!.imageId}.png'
         : null;
 
     final developed = {
-      for (final digest
-          in companyDocs.map((e) => e.developed).expand((e) => e).toList())
+      for (final digest in selectedCompany?.developed ??
+          widget.companyDocs.map((e) => e.developed).expand((e) => e).toList())
         digest.id: LibraryEntry.fromGameDigest(digest)
     };
     final published = {
-      for (final digest
-          in companyDocs.map((e) => e.published).expand((e) => e).toList())
+      for (final digest in selectedCompany?.published ??
+          widget.companyDocs.map((e) => e.published).expand((e) => e).toList())
         if (!developed.containsKey(digest.id))
           digest.id: LibraryEntry.fromGameDigest(digest)
     };
@@ -79,16 +82,19 @@ class CompanyContent extends StatelessWidget {
                 logoUrl != null
                     ? CachedNetworkImage(imageUrl: logoUrl)
                     : Text(
-                        companyDocs.first.name,
+                        widget.companyDocs.first.name,
                         style: const TextStyle(fontSize: 24),
                       ),
                 const SizedBox(width: 64),
-                Flexible(child: Text(companyDocs.first.description ?? '')),
+                Flexible(
+                    child: Text(selectedCompany?.description ??
+                        widget.companyDocs.first.description ??
+                        '')),
               ],
             ),
           ),
         ),
-        if (companyDocs.length > 1)
+        if (widget.companyDocs.length > 1)
           Shelve(
             title: 'Subsidiaries',
             expansion: Padding(
@@ -98,18 +104,29 @@ class CompanyContent extends StatelessWidget {
                   child: ListView(
                     scrollDirection: Axis.horizontal,
                     children: [
-                      for (final company in companyDocs) ...[
+                      for (final company in widget.companyDocs) ...[
                         const SizedBox(width: 64),
                         Column(
                           children: [
-                            SizedBox(
-                              height: 64,
-                              child: company.logo != null
-                                  ? CachedNetworkImage(
-                                      imageUrl:
-                                          '${Urls.imageProvider}/t_logo_med/${company.logo?.imageId}.png',
-                                    )
-                                  : const Icon(Icons.question_mark),
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  selectedCompany = selectedCompany != company
+                                      ? company
+                                      : null;
+                                });
+                              },
+                              child: SizedBox(
+                                height: 64,
+                                child: company.logo != null
+                                    ? CachedNetworkImage(
+                                        imageUrl:
+                                            '${Urls.imageProvider}/t_logo_med/${company.logo?.imageId}.png',
+                                      )
+                                    : const CircleAvatar(
+                                        child: Icon(Icons.question_mark),
+                                      ),
+                              ),
                             ),
                             const Expanded(child: SizedBox.shrink()),
                             Text(
