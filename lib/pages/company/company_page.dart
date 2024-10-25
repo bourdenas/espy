0b18442bcc9e74df4cc244b7b64416c6871e2726
bourdenas <bourdenas@gmail.com>
2024-10-25 14:42:36 +0200

    Add a shelve with subsidiaries in the companies page.

--- lib/pages/company/company_page.dart
+++ lib/pages/company/company_page.dart
@@ -1,4 +1,5 @@
 import 'package:badges/badges.dart' as badges;
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:espy/constants/urls.dart';
 import 'package:espy/modules/documents/igdb_company.dart';
 import 'package:espy/modules/documents/library_entry.dart';
@@ -46,8 +47,9 @@ class CompanyContent extends StatelessWidget {
     companyDocs.sort((l, r) => -(l.developed.length + l.published.length)
         .compareTo(r.developed.length + r.published.length));
 
-    final logoUrl =
-        '${Urls.imageProvider}/t_logo_med/${companyDocs.first.logo?.imageId}.png';
+    final logoUrl = companyDocs.first.logo != null
+        ? '${Urls.imageProvider}/t_logo_med/${companyDocs.first.logo!.imageId}.png'
+        : null;
 
     final developed = {
       for (final digest
@@ -74,18 +76,61 @@ class CompanyContent extends StatelessWidget {
             child: Row(
               children: [
                 const SizedBox(width: 64),
-                Image.network(logoUrl),
+                logoUrl != null
+                    ? CachedNetworkImage(imageUrl: logoUrl)
+                    : Text(
+                        companyDocs.first.name,
+                        style: const TextStyle(fontSize: 24),
+                      ),
                 const SizedBox(width: 64),
                 Flexible(child: Text(companyDocs.first.description ?? '')),
               ],
             ),
           ),
         ),
+        if (companyDocs.length > 1)
+          Shelve(
+            title: 'Subsidiaries',
+            expansion: Padding(
+                padding: const EdgeInsets.all(16),
+                child: SizedBox(
+                  height: 120,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      for (final company in companyDocs) ...[
+                        const SizedBox(width: 64),
+                        Column(
+                          children: [
+                            SizedBox(
+                              height: 64,
+                              child: company.logo != null
+                                  ? CachedNetworkImage(
+                                      imageUrl:
+                                          '${Urls.imageProvider}/t_logo_med/${company.logo?.imageId}.png',
+                                    )
+                                  : const Icon(Icons.question_mark),
+                            ),
+                            const Expanded(child: SizedBox.shrink()),
+                            Text(
+                              company.name,
+                              style: const TextStyle(fontSize: 16.0),
+                            ),
+                            const SizedBox(width: 16),
+                          ],
+                        ),
+                      ]
+                    ],
+                  ),
+                )),
+            color: Colors.amber,
+            expanded: true,
+          ),
         Shelve(
           title: 'Drill-down',
           expansion: LibraryStats(developedModel.entries),
           color: Colors.amber,
-          expanded: true,
+          expanded: false,
         ),
         TileShelve(
           title: 'Developed (${developed.length})',
