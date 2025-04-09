dee43fe19aaa0722ade1c0335c7620b5e6fa46fb
bourdenas <bourdenas@gmail.com>
2025-04-09 23:33:07 +0200

    Remove calendar card elevation.

--- lib/pages/calendar/calendar_card.dart
+++ lib/pages/calendar/calendar_card.dart
@@ -79,7 +79,6 @@ class _CalendarCardState extends State<CalendarCard>
     }
 
     return Material(
-      elevation: 10,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       clipBehavior: Clip.antiAlias,
       child: Stack(
