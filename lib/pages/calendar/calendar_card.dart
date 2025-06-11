cd2f5985e9389837d7a504ccffcbb09beaf58d23
bourdenas <bourdenas@gmail.com>
2025-06-11 23:45:31 +0200

    Refine tile layout for calendar cards.
    Clicking on image covers navigates directly to the game details.
    Clicking on the behind the fold badge expands the games behind the fold.
    
    Known issues: Years view is broken at the moment.

--- lib/pages/calendar/calendar_card.dart
+++ lib/pages/calendar/calendar_card.dart
@@ -2,9 +2,10 @@ import 'package:espy/constants/urls.dart';
 import 'package:espy/modules/documents/game_digest.dart';
 import 'package:espy/pages/calendar/calendar_grid_entry.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:quiver/iterables.dart';
 
-class CalendarCard extends StatefulWidget {
+class CalendarCard extends StatelessWidget {
   const CalendarCard(
     this.calendarEntry, {
     super.key,
@@ -15,28 +16,157 @@ class CalendarCard extends StatefulWidget {
   final List<Widget> overlays;
 
   @override
-  State<CalendarCard> createState() => _CalendarCardState();
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GridTile(
+        child: coverImage(context),
+      ),
+    );
+  }
+
+  Widget coverImage(BuildContext context) {
+    final covers = calendarEntry.getCovers();
+    final behindFoldGames = calendarEntry.digests.length - covers.length;
+    if (covers.length < 4 && !covers.first.isMain) {
+      covers.addAll(range(4 - covers.length)
+          .map((_) => GameDigest(id: 0, name: 'padding')));
+    }
+
+    return ExpandingWidget(
+      scale: 1.1,
+      child: Material(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        clipBehavior: Clip.antiAlias,
+        child: Center(
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              tiledCovers(context, covers),
+              ...overlays,
+              if (behindFoldGames > 0) behindFoldBadge(behindFoldGames),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Positioned behindFoldBadge(int behindFoldGames) {
+    return Positioned(
+      bottom: 8,
+      right: 4,
+      child: ExpandingWidget(
+        scale: 1.2,
+        child: InkWell(
+          onTap: () => calendarEntry.onClick(calendarEntry),
+          child: SizedBox(
+            width: 64,
+            height: 64,
+            child: CircleAvatar(
+              backgroundColor: Color.fromRGBO(0, 0, 0, .7),
+              child: Text('+ $behindFoldGames'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget tiledCovers(BuildContext context, List<GameDigest> covers) {
+    return Column(
+      children: switch (covers.length) {
+        1 => [cardTile(context, covers.first)],
+        2 => [
+            Expanded(
+              child: Row(
+                children: [
+                  for (final digest in covers.take(2)) cardTile(context, digest)
+                ],
+              ),
+            ),
+          ],
+        3 => [
+            Expanded(
+              child: Row(
+                children: [
+                  for (final digest in covers.take(1)) cardTile(context, digest)
+                ],
+              ),
+            ),
+            Flexible(
+              child: Row(
+                children: [
+                  for (final digest in covers.skip(1).take(2))
+                    cardTile(context, digest)
+                ],
+              ),
+            ),
+          ],
+        _ => [
+            Expanded(
+              child: Row(
+                children: [
+                  for (final digest in covers.take(2)) cardTile(context, digest)
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  for (final digest in covers.skip(2).take(2))
+                    cardTile(context, digest)
+                ],
+              ),
+            ),
+          ],
+      },
+    );
+  }
+
+  Widget cardTile(BuildContext context, GameDigest digest) {
+    return Expanded(
+      child: digest.id != 0
+          ? InkWell(
+              onTap: () => context.pushNamed('details',
+                  pathParameters: {'gid': '${digest.id}'}),
+              child: Image.network(
+                '${Urls.imageProvider}/t_cover_big/${digest.cover}.jpg',
+              ),
+            )
+          : Container(),
+    );
+  }
 }
 
-class _CalendarCardState extends State<CalendarCard>
+class ExpandingWidget extends StatefulWidget {
+  final Widget child;
+  final double scale;
+
+  const ExpandingWidget({super.key, required this.child, this.scale = 1.1});
+
+  @override
+  State<ExpandingWidget> createState() => _ExpandingWidgetState();
+}
+
+class _ExpandingWidgetState extends State<ExpandingWidget>
     with SingleTickerProviderStateMixin {
-  bool hover = false;
   late AnimationController controller;
-  late Animation animation;
-  late Animation padding;
+  late Animation<double> animation;
 
   @override
   void initState() {
     super.initState();
+
     controller = AnimationController(
       duration: const Duration(milliseconds: 200),
       vsync: this,
     );
-    animation = Tween(begin: 1.0, end: 1.1).animate(CurvedAnimation(
-        parent: controller, curve: Curves.ease, reverseCurve: Curves.easeIn));
-    padding = Tween(begin: 0.0, end: -12.5).animate(CurvedAnimation(
-        parent: controller, curve: Curves.ease, reverseCurve: Curves.easeIn));
-    controller.addListener(() => setState(() {}));
+    animation = Tween(begin: 1.0, end: widget.scale).animate(CurvedAnimation(
+      parent: controller,
+      curve: Curves.ease,
+      reverseCurve: Curves.easeIn,
+    ));
   }
 
   @override
@@ -47,86 +177,17 @@ class _CalendarCardState extends State<CalendarCard>
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: InkWell(
-        onTap: () => widget.calendarEntry.onClick(widget.calendarEntry),
-        onHover: (val) => setState(() {
-          hover = val;
-          if (hover) {
-            controller.forward();
-          } else {
-            controller.reverse();
-          }
-        }),
-        child: Container(
-          transform: Matrix4(animation.value, 0, 0, 0, 0, animation.value, 0, 0,
-              0, 0, 1, 0, padding.value, padding.value, 0, 1),
-          child: GridTile(
-            child: coverImage(context),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget coverImage(BuildContext context) {
-    final covers = widget.calendarEntry.getCovers();
-    final behindFoldGames = widget.calendarEntry.digests.length - covers.length;
-    if (covers.length < 4 && !covers.first.isMain) {
-      covers.addAll(range(4 - covers.length)
-          .map((_) => GameDigest(id: 0, name: 'padding')));
-    }
-
-    return Material(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      clipBehavior: Clip.antiAlias,
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: [
-          Column(
-            children: [
-              Row(
-                children: [
-                  for (final digest in covers.take(2))
-                    Expanded(
-                      child: digest.id != 0
-                          ? Image.network(
-                              '${Urls.imageProvider}/t_cover_big/${digest.cover}.jpg',
-                            )
-                          : Container(),
-                    )
-                ],
-              ),
-              Row(
-                children: [
-                  for (final digest in covers.skip(2).take(2))
-                    Expanded(
-                      child: digest.id != 0
-                          ? Image.network(
-                              '${Urls.imageProvider}/t_cover_big/${digest.cover}.jpg',
-                            )
-                          : Container(),
-                    )
-                ],
-              ),
-            ],
-          ),
-          ...widget.overlays,
-          if (behindFoldGames > 0)
-            Positioned(
-              bottom: 8,
-              right: 4,
-              child: SizedBox(
-                width: 64,
-                height: 64,
-                child: CircleAvatar(
-                  backgroundColor: Color.fromRGBO(0, 0, 0, .7),
-                  child: Text('+ $behindFoldGames'),
-                ),
-              ),
-            ),
-        ],
+    return MouseRegion(
+      onEnter: (_) => controller.forward(),
+      onExit: (_) => controller.reverse(),
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) {
+          return ScaleTransition(
+            scale: animation,
+            child: widget.child,
+          );
+        },
       ),
     );
   }
